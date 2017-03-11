05ce9cf9cb1e8121346f5fea12f2cc537aa678eb
nem035 <n.stojanovic035@gmail.com>
2017-03-11 14:43:49 -0500

    code preview

--- hello_world.js
+++ hello_world.js
@@ -1,31 +1,31 @@
-/*
-1. Principle: Separate Logic from Drivers (effects).
-
-Logic is functional
-Drivers are imperative
-
-Developer writes the logic, framework drives the effects.
-
-2. Source : input (read) effects
-   Sink   : output (write) effects
-
-3. CycleJS -> Proxies
-
-    How to do?
-
-      a = f(b)
-      b = g(a)
-
-    With proxies:
-
-      bProxy = ...
-      a = f(bProxy)
-      b = g(a)
-      bProxy.imitate(b)
-
-CycleJS is really a loop of read/write effects
-*/
 function script (appContainerSelector, {Cycle, CycleDOM, Rx}) {
+  /*
+  1. Principle: Separate Logic from Drivers (effects).
+
+  Logic is functional
+  Drivers are imperative
+
+  Developer writes the logic, framework drives the effects.
+
+  2. Source : input (read) effects
+    Sink   : output (write) effects
+
+  3. CycleJS -> Proxies
+
+      How to do?
+
+        a = f(b)
+        b = g(a)
+
+      With proxies:
+
+        bProxy = ...
+        a = f(bProxy)
+        b = g(a)
+        bProxy.imitate(b)
+
+  CycleJS is really a loop of read/write effects
+  */
   const { label, input, h1, hr, div, makeDOMDriver } = CycleDOM
 
   function main (sources) {
